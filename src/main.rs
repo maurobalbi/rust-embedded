5e7901d19266d83cb036f79d0c56b216f9c13dae
mauro balbi <maurobalbi@bluewin.ch>
2021-03-27 17:17:44 +0100

    Push button example

--- src/main.rs
+++ src/main.rs
@@ -3,16 +3,16 @@
 
 // pick a panicking behavior
 use panic_halt as _; // you can put a breakpoint on `rust_begin_unwind` to catch panics
-// use panic_abort as _; // requires nightly
-// use panic_itm as _; // logs messages over ITM; requires ITM support
-// use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
+                     // use panic_abort as _; // requires nightly
+                     // use panic_itm as _; // logs messages over ITM; requires ITM support
+                     // use panic_semihosting as _; // logs messages to the host stderr; requires a debugger
 
 use cortex_m::asm;
 use cortex_m_rt::entry;
 use cortex_m_semihosting::hprintln;
 
 use stm32l4::stm32l4x6;
-use stm32l4::stm32l4x6::{tim6};
+use stm32l4::stm32l4x6::tim6;
 
 #[inline(never)]
 fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
@@ -30,7 +30,6 @@ fn delay(tim6: &tim6::RegisterBlock, ms: u16) {
     tim6.sr.modify(|_, w| w.uif().clear_bit());
 }
 
-
 #[entry]
 fn main() -> ! {
     let peripherals = stm32l4x6::Peripherals::take().unwrap();
@@ -40,19 +39,26 @@ fn main() -> ! {
     let rcc = &peripherals.RCC;
 
     rcc.ahb2enr.modify(|_, w| w.gpioaen().set_bit());
+    rcc.ahb2enr.modify(|_, w| w.gpiocen().set_bit());
+
     rcc.apb1enr1.modify(|_, w| w.tim6en().set_bit());
     tim6.cr1.write(|w| w.opm().set_bit().cen().clear_bit());
     tim6.psc.write(|w| w.psc().bits(7_999));
 
     let gpioa = &peripherals.GPIOA;
+    let gpioc = &peripherals.GPIOC;
 
     gpioa.moder.modify(|_, w| w.moder5().output());
-    
+    gpioc.moder.modify(|_, w| w.moder13().input());
+
     let ms = 500;
     loop {
-        gpioa.odr.modify(|_, w| w.odr5().set_bit());
-        delay(tim6, ms);
-        gpioa.odr.modify(|_, w| w.odr5().clear_bit());
-        delay(tim6, ms)
+        let bit = gpioc.idr.read().idr13();
+
+        if bit.bit_is_set() {
+            gpioa.odr.modify(|_, w| w.odr5().set_bit());
+        } else {
+            gpioa.odr.modify(|_, w| w.odr5().clear_bit());
+        }
     }
 }
